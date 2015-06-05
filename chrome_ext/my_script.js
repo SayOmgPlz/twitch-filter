e54b094fe64dc2d7de5713fe6edb758966a414c6
Lutchezar Canov <lecanov@abv.bg>
2015-06-05 20:45:39 +0300

    Chrome extension

---
+++ chrome_ext/my_script.js
@@ -0,0 +1,11 @@
+window.addEventListener ("load", myMain, false);
+
+function myMain (evt) {
+    // DO YOUR STUFF HERE.
+    console.log('loaded');
+    var jsInitChecktimer = setInterval (checkForJS_Finish, 111);
+
+    function checkForJS_Finish () {
+
+    }
+}
